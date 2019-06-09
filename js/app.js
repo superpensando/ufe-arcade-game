67a2095154068a2277c76a6e8a651dc52258aecc
Laura Rodríguez Castillo <laura.rodriguez.castillo@gmail.com>
2019-06-09 13:49:32 +0200

    ufe-project-arcade-game - "Crash" Logica

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
 //Canvas variables
-// to prevent any element of the game from appearing offscreen
 const screenWidth = 500; 
 const onScreenMinWidth = 0; 
-const onScreenMaxWidth =400;
-const onScreenMinHeight=0;  
-const onScreenMaxHeight=400; 
+const onScreenMaxWidth = 400;
+const onScreenMinHeight = 0;  
+const onScreenMaxHeight = 400; 
+const canvasSquareY = 85;
+const canvasSquareX = 100; 
+//Enemy variables
+const enemyX = 0;  
+const enemyY1 = 60;
+const enemySpeed1 = 1+Math.random()*500;
+const enemyY2 = 145;
+const enemySpeed2 = 1+Math.random()*200;
+const enemyY3 = 230;
+const enemySpeed3 = 1+Math.random()*350; 
+//Player variables
 const playerSpeed = 80;
 const playerX = onScreenMaxWidth/2;
 const playerY = onScreenMaxHeight;
@@ -23,6 +33,11 @@ const Enemy = function(x, y, speed) {
     this.speed = speed;
 };
 
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
@@ -30,20 +45,34 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x += this.speed*dt;
-    //If x-axys bug movement off-screen
+    //If x-axys bug movement off-screen, put 'this.x = 0' else continue movement
     this.x = this.x>screenWidth?0:this.x;
-    //this.checkCollisions();
+    this.checkCollisions(this.x, player.x);
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+
 
 Enemy.prototype.checkCollisions = function() {
-   console.log("crash");
-   start();
+   let yPlayer = player.y;
+   let yEnemy = this.y;
+   let xPlayerMin = player.x - 50;
+   let xPlayerMax = player.x + 50;
+   let xEnemy = Math.floor(this.x);
+
+   if ( yPlayer === yEnemy) {
+    if (xEnemy.between(xPlayerMin, xPlayerMax )) {
+        console.log(xEnemy);
+        start();
+    } 
+   } 
 };
+
+Number.prototype.between  = function (a, b) {
+    var min = Math.min.apply(Math, [a,b]),
+        max = Math.max.apply(Math, [a,b]);
+    return this > min && this < max;
+};
+
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -55,8 +84,8 @@ const Player = function(x, y, speed) {
     this.sprite = 'images/char-horn-girl.png';
 };
 
-Player.prototype.update = function(x, y) {
- //Not Player update
+Player.prototype.update = function() {
+  //Don't apply 
 };
 
 Player.prototype.render = function() {
@@ -71,32 +100,31 @@ Player.prototype.reset = function() {
 };
 
 Player.prototype.handleInput = function(keypressed) {
-    console.log(keypressed);
     switch (keypressed) {
         case "up":
         if (this.y > onScreenMinHeight ){
-           this.y -=90;
-           if (this.y == -50) {
+           this.y -=canvasSquareY;
+           if (this.y == -25) {
                 wins(); 
            }
-            console.log("y up: "+this.y);
+            //console.log("y up: "+this.y);
         }
         break;
         case "down":
         if (this.y < onScreenMaxHeight ){
-            this.y +=90; 
+            this.y +=canvasSquareY; 
             //console.log("y down: "+this.y);
         }
         break;
         case "left":
         if (this.x > onScreenMinWidth){
-            this.x -=100;
+            this.x -=canvasSquareX;
             //console.log(this.x);
         }
         break;
         case "right":
         if (this.x < onScreenMaxWidth){
-            this.x +=100; 
+            this.x +=canvasSquareX; 
             //console.log(this.x);
         }
         break;
@@ -111,7 +139,7 @@ Player.prototype.handleInput = function(keypressed) {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var allEnemies = [];
-var player = new Player();
+var player = new Player(playerX,playerY,playerSpeed);
 
 
 // This listens for key presses and sends the keys to your
@@ -132,9 +160,9 @@ function start() {
     player.reset();
     allEnemies = [];
     allEnemies.push(
-        new Enemy(0,50, 1+Math.random()*500),
-        new Enemy(0,150, 1+Math.random()*200),
-        new Enemy(0,230, 1+Math.random()*300),
+        new Enemy(enemyX,enemyY1,enemySpeed1),
+        new Enemy(enemyX,enemyY2,enemySpeed2),
+        new Enemy(enemyX,enemyY3,enemySpeed3),
     );
 }; 
 
